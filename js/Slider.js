f3c59079dc86ebe411177f2487c59ba90c95a6fc
Muhammedshahir001 <muhammedshahir91@gmail.com>
2024-03-02 10:52:52 +0530

    intial commit

---
+++ js/Slider.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".teachers-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 1,
+
+        },
+        768: {
+            slidesPerView: 2,
+
+        },
+        1024: {
+            slidesPerView: 3,
+
+        },
+    },
+});
